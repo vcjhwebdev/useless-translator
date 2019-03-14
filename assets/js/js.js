24a4f0d538937c7587fa39fbce1058d5c9f5a72f
Nathan Weng <nathan.weng@warriorlife.net>
2019-03-14 09:09:27 -0700

    dsfsf

--- assets/js/js.js
+++ assets/js/js.js
@@ -223,3 +223,13 @@ function encodeToBin() {
   }
   document.ToBin.codeboxBin.value+="\n\n\Explanation:\n\n"+tempToBin
 }
+function binaryAgent(str) {
+
+var newBin = str.split(" ");
+var binCode = [];
+
+for (i = 0; i < newBin.length; i++) {
+    binCode.push(String.fromCharCode(parseInt(newBin[i], 2)));
+  }
+return binCode.join("");
+}
