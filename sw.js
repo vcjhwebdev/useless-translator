e8dcbc63d89ccd6b11f7bbf145f6e863df48ce02
mac1 <mac1@133imac14.valleychristian.net>
2019-03-26 08:37:17 -0700

    service

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'v0.1';
+const CACHE_NAME = 'v1.0';
 
 var urlsToCache = [
 	'index.html',
-	'assets/css/style.css',
-	'assets/js/calculator.js'
+	'assets/css/styles.css',
+	'assets/js/js.js'
 ];
 
 self.addEventListener('install', function(event) {
